7b2e7133d1ad74da77a05ebf0ee74388182e8578
Dmitriy Skudarnov <skrdvx@gmail.com>
2016-07-27 18:16:01 +0300

    included wp_query posts in home slider

--- page-home.php
+++ page-home.php
@@ -6,69 +6,95 @@
 
 <?php get_header(); ?>
 
-			<div id="content">
+<main id="main" role="main">
 
-						<main id="main" role="main">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php
+		// WP_Query arguments
+		$args = array (
+			'cat' => '1',
+		);
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+		// The Query
+		$the_query = new WP_Query( $args );
+
+		// The Loop
+/*
+	echo '<ul>';
+
+		$the_query->the_post();
+		echo '<li>' . get_the_title() . '</li>';
+		echo '<a href="'. the_permalink().'" rel="bookmark" title="">'. the_title() .'</a>';
+		$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full', false, '' );
+		echo $src[0];
+
+	echo '</ul>';
+*/
+
+	/* Restore original Post Data */
 
 
-								<section class="cf" itemprop="articleBody">
 
-									<div id="fullpage">
-										<div class="section" id="section0">
-											<?php for ($i=0; $i < 11 ; $i++) { ?>
-												<?php if( get_field('case_'.$i.'_image') ) { ?>
-													<div class="slide" id="slide<?php echo $i; ?>"
-															 style="background-image: url(<?php the_field('case_'.$i.'_image'); ?>);">
-														<a class="case-link" href="<?php the_field('case_'.$i.'_link'); ?>"  rel="bookmark">
-															<?php the_field('case_'.$i.'_title'); ?>
-														</a>
-													</div>
-												<?php } ?>
-											<?php } ?>
-										</div>
-										<div class="section " id="section1">
-											<h1>Услуги</h1>
-										</div>
-										<div class="section" id="section2">
-											<h1>Команда</h1>
-										</div>
-										<div class="section" id="section3">
-											<h1>Контакты</h1>
-										</div>
-									</div>
+?>
 
-									<?php the_content(); ?>
 
-								</section>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
 
-								<footer class="article-footer">
+			<div id="fullpage">
+				<section class="section" id="section-cases">
+					<?php /* for ($i=0; $i < 11 ; $i++) { ?>
+						<?php if( get_field('case_'.$i.'_image') ) { ?>
+							<div class="slide" id="slide<?php echo $i; ?>"
+									 style="background-image: url(<?php the_field('case_'.$i.'_image'); ?>);">
+								<a class="case-link" href="<?php the_field('case_'.$i.'_link'); ?>"  rel="bookmark">
+									<?php the_field('case_'.$i.'_title'); ?>
+								</a>
+							</div>
+						<?php } ?>
+					<?php } */ ?>
+					<?php
+					$the_query->the_post();
+					$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full', false, '' );
+					echo '<div class="slide" style="background-image: url('. $src[0] .');">';
+					echo '<a class="case-link" href="'. get_the_permalink().'" rel="bookmark" title="">'. get_the_title() .'</a>';
+					echo '</div>';
 
-								</footer>
+						wp_reset_postdata();
+					?>
 
-							</article>
-
-							<?php endwhile; else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</main>
-
+				</section>
+				<section class="section " id="section1">
+					<h1>Услуги</h1>
+				</section>
+				<section class="section" id="section2">
+					<h1>Команда</h1>
+				</section>
+				<section class="section" id="section3">
+					<h1>Контакты</h1>
+				</section>
 			</div>
+
+			<?php the_content(); ?>
+
+	</article>
+
+	<?php endwhile; else : ?>
+
+			<article id="post-not-found" class="hentry cf">
+					<header class="article-header">
+						<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+				</header>
+					<section class="entry-content">
+						<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+				</section>
+				<footer class="article-footer">
+						<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
+				</footer>
+			</article>
+
+	<?php endif; ?>
+
+</main>
 
 <?php get_footer(); ?>
